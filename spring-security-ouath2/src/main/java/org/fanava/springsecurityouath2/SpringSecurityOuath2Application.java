05aa2b9360267807d730e7d1fd18c2bf0e28eb78
Saeed <rabiee.saeid@yahoo.com>
2025-03-21 21:11:43 +0330

    spring security layer added

---
+++ spring-security-ouath2/src/main/java/org/fanava/springsecurityouath2/SpringSecurityOuath2Application.java
@@ -0,0 +1,13 @@
+package org.fanava.springsecurityouath2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOuath2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOuath2Application.class, args);
+	}
+
+}
---
+++ spring-security-ouath2/src/test/java/org/fanava/springsecurityouath2/SpringSecurityOuath2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fanava.springsecurityouath2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOuath2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
